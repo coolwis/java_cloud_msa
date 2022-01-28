0390b1954f3df62475e091b81a906302cc35fcdc
coolwis <wisdombrave@gmail.com>
2022-01-28 14:05:26 +0900

    gateway

--- cloud/gateway/src/main/java/com/psc/cloud/gateway/GatewayApplication.java
+++ cloud/gateway/src/main/java/com/psc/cloud/gateway/GatewayApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
